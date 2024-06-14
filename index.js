ebd2cbd2f43cfcf21484071df724c0f271ff3835
Rockrayhan <khayrulalamdict@gmail.com>
2024-06-14 10:20:30 +0600

    stripe processing

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // env config
 const env = require("dotenv");
 env.config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000 ;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 app.use(cors());
 app.use(express.json());
 
@@ -25,7 +26,6 @@ const uri = process.env.DB_URI;
 
 // const uri="mongodb+srv://khayrulalamdict:AdrN0sAmVR43ne6K@cluster0.qdsaagu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -196,7 +196,24 @@ app.patch('/user/:email', async(req, res) => {
 })
 
 
+// ============= payment intent ================
 
+app.post('/create-payment-intent', async(req, res) => {
+  const {price} = req.body ;
+  const amount = parseInt(price * 100 ) ;
+
+   // Create a PaymentIntent with the order amount and currency
+   const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "usd",
+    payment_method_types: ['card']
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+
+})
 
 
     console.log("Database is connected");
